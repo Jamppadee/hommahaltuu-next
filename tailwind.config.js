49a757fc9842a1d62ab3c48ff65065344ac7953b
Jamppadee <114110932+Jamppadee@users.noreply.github.com>
2025-10-01 12:53:07 -0700

    Remove Tailwind plugin imports from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,5 @@ module.exports = {
     './components/**/*.{ts,tsx}'
   ],
   theme: { extend: {} },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')]
+     plugins: [],
 }
