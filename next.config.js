313fc7d7f9cc95cbed7994718e6348470f12b135
Jamppadee <114110932+Jamppadee@users.noreply.github.com>
2025-10-02 17:28:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { tsconfigPaths: true },
-  images: { domains: ['images.unsplash.com', 'randomuser.me'] }
-}
-module.exports = nextConfig
+  images: {
+    domains: ['images.unsplash.com', 'randomuser.me'],
+  },
+};
+module.exports = nextConfig;
